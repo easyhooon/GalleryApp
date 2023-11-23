720b37790a1cded1cf0be8fd44a1eb389353aa19
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-24 06:44:56 +0900

    Loading, Error 화면 추가 및 상태에 따른 화면 분기 처리

---
+++ presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/ErrorScreen.kt
@@ -0,0 +1,46 @@
+package com.daangn.leejihun.gallery.presentation.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.daangn.leejihun.gallery.presentation.R
+import com.daangn.leejihun.gallery.presentation.ui.theme.TextLMedium
+import com.daangn.leejihun.gallery.presentation.ui.theme.Title
+
+@Composable
+fun ErrorScreen(
+    modifier: Modifier = Modifier,
+    onClickRetryButton: () -> Unit,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = stringResource(id = R.string.error_message),
+                style = Title,
+            )
+            Spacer(modifier.height(12.dp))
+            Button(onClick = onClickRetryButton) {
+                Text(
+                    text = stringResource(id = R.string.retry),
+                    style = TextLMedium,
+                )
+            }
+        }
+    }
+}
--- presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryScreen.kt
+++ presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemContentType
 import androidx.paging.compose.itemKey
@@ -22,20 +23,29 @@ fun GalleryScreen(
 ) {
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
 
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = min(screenWidth, 160.dp)),
-        modifier = modifier.fillMaxSize(),
-        contentPadding = PaddingValues(4.dp),
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-    ) {
-        items(
-            count = photoList.itemCount,
-            key = photoList.itemKey(key = { it.id }),
-            contentType = photoList.itemContentType(),
-        ) { index ->
-            photoList[index]?.let { photo ->
-                PhotoCard(photo = photo)
+    val isLoading = photoList.loadState.refresh is LoadState.Loading
+    val isError = photoList.loadState.refresh is LoadState.Error
+
+    when {
+        isLoading -> LoadingScreen()
+        isError -> ErrorScreen(onClickRetryButton = { photoList.retry() })
+        else -> {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = min(screenWidth, 160.dp)),
+                modifier = modifier.fillMaxSize(),
+                contentPadding = PaddingValues(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+                items(
+                    count = photoList.itemCount,
+                    key = photoList.itemKey(key = { it.id }),
+                    contentType = photoList.itemContentType(),
+                ) { index ->
+                    photoList[index]?.let { photo ->
+                        PhotoCard(photo = photo)
+                    }
+                }
             }
         }
     }
