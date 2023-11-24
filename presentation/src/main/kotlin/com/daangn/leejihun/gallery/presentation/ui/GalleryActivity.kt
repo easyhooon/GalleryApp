59b94b3f1d5f7c6c9ccaf7b525a0f05917276a85
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-24 18:11:28 +0900

    SplashScreen API 적용

--- presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryActivity.kt
+++ presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        installSplashScreen()
         enableEdgeToEdge()
 
         setContent {
