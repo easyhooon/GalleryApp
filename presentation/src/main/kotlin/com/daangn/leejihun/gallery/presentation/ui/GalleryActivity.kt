a1bea5f9231c48af41c84f4bce77c78a89f7fa2f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-24 15:45:28 +0900

    Window Insets 활성화

--- presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryActivity.kt
+++ presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryActivity.kt
@@ -3,12 +3,16 @@ package com.daangn.leejihun.gallery.presentation.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             GalleryApp()
         }
