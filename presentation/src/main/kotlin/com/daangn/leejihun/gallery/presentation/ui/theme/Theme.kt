3adb415e07db4795a8a21c955e9b6e61225609d2
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-25 04:39:58 +0900

    StatusBar, NavigationBar 색상 변경

--- presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/theme/Theme.kt
+++ presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/theme/Theme.kt
@@ -56,8 +56,9 @@ fun GalleryAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
