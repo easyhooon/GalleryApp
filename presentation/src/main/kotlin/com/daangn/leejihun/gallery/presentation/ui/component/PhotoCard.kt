b1663f31f67bb2caf593f5dfeeaa2c8fcb4e272c
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-11-26 06:59:09 +0900

    Photo Card 내에 작가 이름을 하단에 추가

--- presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/component/PhotoCard.kt
+++ presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/component/PhotoCard.kt
@@ -1,20 +1,31 @@
 package com.daangn.leejihun.gallery.presentation.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.daangn.leejihun.gallery.presentation.model.Photo
 import com.daangn.leejihun.gallery.presentation.R
+import com.daangn.leejihun.gallery.presentation.model.Photo
+import com.daangn.leejihun.gallery.presentation.ui.theme.Gray900
+import com.daangn.leejihun.gallery.presentation.ui.theme.TextXsRegular
 
 @Composable
 fun PhotoCard(
@@ -32,12 +43,29 @@ fun PhotoCard(
                 onPhotoClick(photo)
             },
     ) {
-        AsyncImage(
-            model = ImageRequest.Builder(context)
-                .data(photo.downloadUrl)
-                .build(),
-            contentDescription = stringResource(id = R.string.photo_image),
-            contentScale = ContentScale.Crop,
-        )
+        Box {
+            AsyncImage(
+                model = ImageRequest.Builder(context)
+                    .data(photo.downloadUrl)
+                    .build(),
+                contentDescription = stringResource(id = R.string.photo_image),
+                contentScale = ContentScale.Crop,
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(Gray900.copy(alpha = 0.6f))
+                    .align(Alignment.BottomStart)
+                    .padding(start = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    color = Color.White,
+                    text = photo.author,
+                    style = TextXsRegular,
+                )
+            }
+        }
     }
 }
