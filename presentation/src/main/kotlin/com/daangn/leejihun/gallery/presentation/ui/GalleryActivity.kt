506eb1012cdde0f37b8e13e612cba0cb2f83d605
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-12-09 18:40:32 +0900

    사용하지 않는 코드 제거
    Window Insets API 활성화 할 필요 x

--- presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryActivity.kt
+++ presentation/src/main/kotlin/com/daangn/leejihun/gallery/presentation/ui/GalleryActivity.kt
@@ -3,7 +3,6 @@ package com.daangn.leejihun.gallery.presentation.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,7 +11,6 @@ class GalleryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
 
         setContent {
             GalleryApp()
